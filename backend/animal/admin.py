350c640e67154548e65c897662cf8bb47fd2fa60
Arthur <arthurmarquesazevedo@gmail.com>
2024-12-10 10:43:59 -0300

    Remove arquivos e configurações do aplicativo de gerenciamento de usuários e animais

--- backend/animal/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Sex, Status, Species, Breed, Animal
-
-admin.site.register(Sex)
-admin.site.register(Status)
-admin.site.register(Species)
-admin.site.register(Breed)
-admin.site.register(Animal)
