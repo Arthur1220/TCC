d5fcc30a126d6c1e66bdb3103bbd7d4e7fc45f99
Arthur <arthurmarquesazevedo@gmail.com>
2025-07-08 19:34:23 -0300

    Add PostgreSQL database service to production setup
    - Introduced a new 'db' service using the PostgreSQL 15 image.
    - Added volume for persistent database storage.
    - Updated 'backend' service to depend on the new 'db' service.
    - Cleaned up comments and ensured environment variables are loaded from .env file.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,11 +119,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
+    )
 }
 
 
