1ade9233fe38e342e89e789765b4fa327a68b4a6
Arthur <arthurmarquesazevedo@gmail.com>
2025-07-09 15:01:39 -0300

    fix: Corrigir URL do RPC para o ambiente local no hardhat.config.js

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     local: {
-      url: process.env.RPC_URL,        // http://localhost:8545
+      url: 'http://127.0.0.1:8545',        // http://localhost:8545
       chainId: 31337,
       accounts: [process.env.WALLET_PRIVATE]
     }
